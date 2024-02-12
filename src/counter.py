c52d53cba019eafea77b05621c68ca4fcb604136
VyProductions <VyProductions.music@gmail.com>
2024-02-12 14:51:14 -0800

    Fixing linter responses for counter.py

--- src/counter.py
+++ src/counter.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
@@ -18,17 +19,19 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """Update a counter"""
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
 
-    if not name in COUNTERS:
+    if name not in COUNTERS:
         return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
 
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
@@ -36,7 +39,7 @@ def read_counter(name):
     app.logger.info(f"Request to read counter: {name}")
     global COUNTERS
 
-    if not name in COUNTERS:
+    if name not in COUNTERS:
         return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
-    
+
     return {name: COUNTERS[name]}, status.HTTP_200_OK
